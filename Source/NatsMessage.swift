2660f09bb701bef8eff2233ada561cb806fd63e2
Ray Krow <raykrow@Rays-MacBook-Pro.local>
2018-03-12 19:33:06 -0700

    fix NatsMessage access levels

--- Source/NatsMessage.swift
+++ Source/NatsMessage.swift
@@ -9,11 +9,11 @@ import Foundation
 
 public struct NatsMessage {
 
-    let payload: String?
-    let byteCount: UInt32?
-    let subject: NatsSubject
-    let replySubject: NatsSubject?
-    let mid: String
+    public let payload: String?
+    public let byteCount: UInt32?
+    public let subject: NatsSubject
+    public let replySubject: NatsSubject?
+    public let mid: String
     
     init(payload: String?, byteCount: UInt32?, subject: NatsSubject, replySubject: NatsSubject? = nil) {
         self.payload = payload
@@ -26,23 +26,23 @@ public struct NatsMessage {
 }
 
 extension NatsMessage {
-    public static func publish(payload: String, subject: String) -> String {
+    internal static func publish(payload: String, subject: String) -> String {
         guard let data = payload.data(using: String.Encoding.utf8) else { return "" }
         return "\(NatsOperation.publish.rawValue) \(subject) \(data.count)\r\n\(payload)\r\n"
     }
-    public static func subscribe(subject: String, sid: String, queue: String = "") -> String {
+    internal static func subscribe(subject: String, sid: String, queue: String = "") -> String {
         return "\(NatsOperation.subscribe.rawValue) \(subject) \(queue) \(sid)\r\n"
     }
-    public static func unsubscribe(sid: String) -> String {
+    internal static func unsubscribe(sid: String) -> String {
         return "\(NatsOperation.unsubscribe.rawValue) \(sid)\r\n"
     }
-    public static func pong() -> String {
+    internal static func pong() -> String {
         return "\(NatsOperation.pong.rawValue)\r\n"
     }
-    public static func ping() -> String {
+    internal static func ping() -> String {
         return "\(NatsOperation.ping.rawValue)\r\n"
     }
-    public static func connect(config: [String:Any]) -> String {
+    internal static func connect(config: [String:Any]) -> String {
         guard let data = try? JSONSerialization.data(withJSONObject: config, options: []) else { return "" }
         guard let payload = data.toString() else { return "" }
         return "\(NatsOperation.connect.rawValue) \(payload)\r\n"
