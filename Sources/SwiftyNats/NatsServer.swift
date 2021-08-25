755de376818d27f9fd76a320c641e4d514928b6f
Kris Simon <kris@ausdertechnik.de>
2021-08-25 23:47:13 +0200

    Provide server default values and add new values

--- Sources/SwiftyNats/NatsServer.swift
+++ Sources/SwiftyNats/NatsServer.swift
@@ -2,34 +2,49 @@
 //  NatsServer.swift
 //  SwiftyNats
 //
-//  Created by Ray Krow on 2/27/18.
+//  Created by Kris Simon on 8/25/21.
 //
+import Foundation
 
 struct NatsServer {
     
+    let host: String
+    let clientIp: String
+    let clientId: UInt
+    let port: UInt
+    let serverName: String
     let serverId: String
+    let maxPayload: UInt
     let version: String
     let go: String
-    let host: String
-    let port: UInt
-    let authRequired: Bool
-    let sslRequired: Bool
-    let maxPayload: UInt
-    
+    let proto: UInt
+    let gitCommit: String?
+
+    let authRequired: Bool?
+    let sslRequired: Bool?
+
     let connectUrls: [String]?
     let tlsVerify: Bool?
     
     init(_ data: [String: AnyObject]) {
-        self.serverId = data["server_id"] as! String
-        self.version = data["version"] as! String
-        self.go = data["go"] as! String
         self.host = data["host"] as! String
+        self.clientIp = data["client_ip"] as? String ?? ""
+        self.clientId = data["client_id"] as? UInt ?? 0
         self.port = data["port"] as! UInt
-        self.authRequired = data["auth_required"] as! Bool
-        self.sslRequired = data["ssl_required"] as! Bool
-        self.maxPayload = data["max_payload"] as! UInt
-        self.tlsVerify = data["tls_verify"] as? Bool
+        self.serverName = data["server_name"] as? String ?? ""
+        self.serverId = data["server_id"] as? String ?? UUID().uuidString
+        self.maxPayload = data["max_payload"] as? UInt ?? 0
+        self.version = data["version"] as? String ?? "0.0.0"
+        self.go = data["go"] as? String ?? ""
+        self.proto = data["proto"] as? UInt ?? 0
+        self.gitCommit = data["git_commit"] as? String ?? ""
+        
+        self.authRequired = data["auth_required"] as? Bool ?? false
+        self.sslRequired = data["ssl_required"] as? Bool ?? false
+        
         self.connectUrls = data["connect_urls"] as? [String]
+        self.tlsVerify = data["tls_verify"] as? Bool
+        
     }
     
 }
